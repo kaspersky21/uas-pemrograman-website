e9024c9035cb7ee8464fe6a2a39cc9c07614ed4b
unknown <ilfanza212@gmail.com>
2023-12-21 21:14:11 +0700

    update

--- search-silver.php
+++ search-silver.php
@@ -47,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Logis Bootstrap Template - Pricing</title>
+  <title>Jakarta (Silver)</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
